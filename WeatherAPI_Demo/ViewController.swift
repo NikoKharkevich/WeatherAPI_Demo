718af0123420dc08f12fa81e4562e0a220f898cc
Nikola Kharkevich <niko.kharkevych@gmail.com>
2022-01-16 21:57:46 +0200

    Initial Commit

---
+++ WeatherAPI_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAPI_Demo
+//
+//  Created by Nikola Kharkevich on 16.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
